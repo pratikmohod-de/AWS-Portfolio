ca99f4d11c9188a1d42987c18f71dbd1af0712e4
pratikmohod-de <pratik.mohod@peopletech.com>
2025-02-28 16:55:10 +0530

    Adding Palindrome

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+def is_palindrome(s):
+    return s == s[::-1]
+
+# Test the function
+input_string = "madam"
+if is_palindrome(input_string):
+    print(f'"{input_string}" is a palindrome.')
+else:
+    print(f'"{input_string}" is not a palindrome.')
